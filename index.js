30ebfb88977e3c249794cfe0fa9f88f101f5e504
Vladyslav <vkadyrov@protonmail.com>
2022-09-21 13:06:46 -0400

    commit tree 274cf75feaae042ec132a310962a33693c200d08

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(currentLocation) {
+    let hqLocation = 42;
+    return(Math.abs(hqLocation - currentLocation));
+}
+
+function distanceFromHqInFeet(currentLocation) {
+    let hqLocation = 42
+    distanceFromHqInBlocks(currentLocation);
+    return(Math.abs((hqLocation - currentLocation) * 264))
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return(Math.abs((destination - start) * 264))
+}
+
+function calculatesFarePrice(start, destination) {
+    distanceTravelledInFeet(start, destination)
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return(Math.abs(distanceTravelledInFeet(start, destination) * 0));
+    } else if (distanceTravelledInFeet(start, destination) >= 400, distanceTravelledInFeet(start, destination) < 2000) {
+        return (Math.abs((distanceTravelledInFeet(start, destination) - 400) * 0.02));          
+    } else if (distanceTravelledInFeet(start, destination) > 2000, distanceTravelledInFeet(start, destination) < 2500) {
+        return(25);
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return("cannot travel that far")
+    }
+}
